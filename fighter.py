7c6b197e55cdd4897aba244795a355734d7f6ab8
Alan Gardner <urfolomeus@gmail.com>
2023-10-08 15:51:50 +0100

    Only jump once

--- fighter.py
+++ fighter.py
@@ -4,6 +4,7 @@ class Fighter():
     def __init__(self, x, y):
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
+        self.jump = False
 
     def move(self, screen_width, screen_height):
         SPEED = 10
@@ -20,8 +21,9 @@ class Fighter():
         if key[pygame.K_d]:
             dx = SPEED
         #jump
-        if key[pygame.K_w]:
+        if key[pygame.K_w] and not self.jump:
             self.vel_y = -30
+            self.jump = True
 
         # apply gravity
         self.vel_y += GRAVITY
@@ -34,6 +36,7 @@ class Fighter():
             dx = screen_width - self.rect.right
         if self.rect.bottom + dy > screen_height - 110:
             self.vel_y = 0
+            self.jump = False
             dy = screen_height - 110 - self.rect.bottom
 
         # update player position
@@ -41,4 +44,4 @@ class Fighter():
         self.rect.y += dy
 
     def draw(self, surface):
-        pygame.draw.rect(surface, (255,0,0), self.rect)
+        pygame.draw.rect(surface, (255, 0, 0), self.rect)
