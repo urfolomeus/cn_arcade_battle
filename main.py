c2a019d24b4f01721267a5b8ac6180a385920235
Alan Gardner <urfolomeus@gmail.com>
2023-10-08 15:22:08 +0100

    First pass at fighters

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from pygame.locals import *
+
+from fighter import Fighter
 
 pygame.init()
 
@@ -12,16 +13,26 @@ pygame.display.set_caption("Cartoon Network Arcade Battle")
 # load bg image
 bg_image = pygame.image.load("./assets/images/background/background.jpg").convert_alpha()
 
+
 # function for drawing background
 def draw_bg():
     scaled_bg = pygame.transform.scale(bg_image, (SCREEN_OPTS_WIDTH, SCREEN_OPTS_HEIGHT))
     screen.blit(scaled_bg, (0, 0))
 
+
+# create two instances of fighter
+fighter_1 = Fighter(200, 310)
+fighter_2 = Fighter(700, 310)
+
 # game loop
 run = True
-while run :
+while run:
     # draw background
     draw_bg()
+
+    # draw fighters
+    fighter_1.draw(screen)
+    fighter_2.draw(screen)
 
     # event handler
     for event in pygame.event.get():
